8ecefd1c915c30811e6617666f9e8e939d5586a2
Unknown <hsinying.lin@stonybrook.edu>
2018-11-30 18:23:21 -0500

    move
    move the time defs to the front
    add parse method calls in the end

--- cse331.py
+++ cse331.py
@@ -42,154 +42,6 @@ def unblock_ip(ip):
     cmd="iptables -D INPUT -s "+ip+" -j DROP"
     subprocess.call(cmd,shell=True)
 
-
-   
-        
-
-###########READ FILE############
-log_file_path = r"/var/log/apache2/access.log" #webserver log
-ssh_log_file =  r"/var/log/auth.log" #SSH LOG
-
-dict_check = {}
-
-
-
-
-parse_IP = '(\d+\.\d+\.\d+\.\d+)'
-parse_WP = '(\"POST\s.+200)'
-parse_php = '(mysql-denied)'
-parse_jm = '(GET \/index\.php\/component\/users\/\?view=login&Itemid=101)'
-parse_ssh = '(Invalid)'
-
-parse_time = '(\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2})'
-parse_ssh_time = '(\w{3} \d{2} \d{2}:\d{2}:\d{2})'
-
-
-def parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP):
-    #match_list = []
-    notin_flag = False
-    with open(log_file_path, "r") as file:
-        for line in file:
-            for match in re.finditer(parse_WP, line, re.S):
-                match_text = match.group()
-                # match_list.append(match_text)
-                search_IP = re.search(parse_IP, line, re.S)
-                match_IP = search_IP.group()
-                ##### PARSE THE TIME######
-                search_time = re.search(parse_time, line, re.S)
-                time_tried = search_time.group()
-                time_tried = convertTime(time_tried)
-                ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
-                for client in client_list:
-                    if client.ip_address == match_IP:
-                        client.fail_requests + 1
-                        notin_flag = True
-                if notin_flag == False:
-                    newclient = Client(match_IP, time_tried, 1, None)
-                    client_list.append(newclient)
-                notin_flag = False
-
-                ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
-                ##### CALL BLCOK IP METHOD #####
-                for client in client_list:
-                    if client.fail_requests >= request:
-                        block_ip(client.ip_address)
-                        client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
-                # ip_dict(match_IP)
-
-            for match in re.finditer(parse_jm, line, re.S):
-                match_text = match.group()
-                 #match_list.append(match_text)
-                search_IP = re.search(parse_IP, line, re.S)
-                match_IP = search_IP.group()
-                ##### PARSE THE TIME######
-                search_time = re.search(parse_time, line, re.S)
-                time_tried = search_time.group()
-                time_tried = convertTime(time_tried)
-                ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
-                for client in client_list:
-                    if client.ip_address == match_IP:
-                        client.fail_requests + 1
-                        notin_flag = True
-                if notin_flag == False:
-                    newclient = Client(match_IP, time_tried, 1, None)
-                    client_list.append(newclient)
-                notin_flag = False
-
-                    ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
-                    ##### CALL BLCOK IP METHOD #####
-                for client in client_list:
-                    if client.fail_requests >= request:
-                        block_ip(client.ip_address)
-                        client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
-                    #ip_dict(match_IP)
-
-
-            for match in re.finditer(parse_php, line, re.S):
-                match_text = match.group()
-                #match_list.append(match_text)
-                search_IP = re.search(parse_IP, line, re.S)
-                match_IP = search_IP.group()
-                ##### PARSE THE TIME######
-                search_time = re.search(parse_time, line, re.S)
-                time_tried = search_time.group()
-                time_tried = convertTime(time_tried)
-                ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
-                for client in client_list:
-                    if client.ip_address == match_IP:
-                        client.fail_requests + 1
-                        notin_flag = True
-                if notin_flag == False:
-                    newclient = Client(match_IP, time_tried, 1, None)
-                    client_list.append(newclient)
-                notin_flag = False
-
-                    ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
-                    ##### CALL BLCOK IP METHOD #####
-                for client in client_list:
-                    if client.fail_requests >= request:
-                        block_ip(client.ip_address)
-                        client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
-                    #ip_dict(match_IP)
-    file.close()
-
-def pareseSSHData(ssh_log_file, parse_ssh):
-    #match_list = []
-    notin_flag = False
-    with open(ssh_log_file, "r") as file:
-    for line in file:
-        for match in re.finditer(parse_ssh, line, re.S):
-            match_text = match.group()
-            #match_list.append(match_text)
-            search_IP = re.search(parse_IP, line, re.S)
-            match_IP = search_IP.group()
-            #print(match_IP)
-            ##### PARSE THE TIME######
-            search_time = re.search(parse_ssh_time, line, re.S)
-            time_tried = search_time.group()
-            time_tried = SSH_datetime(time_tried)
-            #print(time_tried)
-            ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
-            for client in client_list:
-                if client.ip_address == match_IP:
-                    client.fail_requests + 1
-                    notin_flag = True
-            if notin_flag == False:
-                newclient = Client(match_IP, time_tried, 1, None)
-                client_list.append(newclient)
-            notin_flag = False
-
-            ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
-            ##### CALL BLOCK IP METHOD #####
-            for client in client_list:
-                #print(client.fail_requests)
-                if client.fail_requests >= request:
-                    block_ip(client.ip_address)
-                    client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
-            #ip_dict(match_IP)
-            #print match_list
-file.close()
-
 def checkMonth(Mon):
     if Mon == 'Jan':
         return '1'
@@ -268,11 +120,167 @@ def SSH_datetime(input):
     result='2018-'+month+'-'+timeList[1]
     return result
 
+   
+        
+
+###########READ FILE############
+log_file_path = r"/var/log/apache2/access.log" #webserver log
+ssh_log_file =  r"/var/log/auth.log" #SSH LOG
+
+dict_check = {}
+
+
+
+
+parse_IP = '(\d+\.\d+\.\d+\.\d+)'
+parse_WP = '(\"POST\s.+200)'
+parse_php = '(mysql-denied)'
+parse_jm = '(GET \/index\.php\/component\/users\/\?view=login&Itemid=101)'
+parse_ssh = '(Invalid)'
+
+parse_time = '(\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2})'
+parse_ssh_time = '(\w{3} \d{2} \d{2}:\d{2}:\d{2})'
+
+
+def parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP):
+    #match_list = []
+    notin_flag = False
+    with open(log_file_path, "r") as file:
+        for line in file:
+            for match in re.finditer(parse_WP, line, re.S):
+                match_text = match.group()
+                # match_list.append(match_text)
+                search_IP = re.search(parse_IP, line, re.S)
+                match_IP = search_IP.group()
+                ##### PARSE THE TIME######
+                search_time = re.search(parse_time, line, re.S)
+                time_tried = search_time.group()
+                time_tried = convertTime(time_tried)
+                ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
+                for client in client_list:
+                    if client.ip_address == match_IP:
+                        client.fail_requests + 1
+                        notin_flag = True
+                if notin_flag == False:
+                    newclient = Client(match_IP, time_tried, 1, None)
+                    client_list.append(newclient)
+                notin_flag = False
+
+                ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
+                ##### CALL BLCOK IP METHOD #####
+                for client in client_list:
+                    if client.fail_requests >= request:
+                        client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
+                        splite_last_dec = client.time_blocked.rpartition('.')
+                        client.time_blocked = splite_last_dec[0]
+                # ip_dict(match_IP)
+
+            for match in re.finditer(parse_jm, line, re.S):
+                match_text = match.group()
+                 #match_list.append(match_text)
+                search_IP = re.search(parse_IP, line, re.S)
+                match_IP = search_IP.group()
+                ##### PARSE THE TIME######
+                search_time = re.search(parse_time, line, re.S)
+                time_tried = search_time.group()
+                time_tried = convertTime(time_tried)
+                ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
+                for client in client_list:
+                    if client.ip_address == match_IP:
+                        client.fail_requests + 1
+                        notin_flag = True
+                if notin_flag == False:
+                    newclient = Client(match_IP, time_tried, 1, None)
+                    client_list.append(newclient)
+                notin_flag = False
+
+                    ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
+                    ##### CALL BLCOK IP METHOD #####
+                for client in client_list:
+                    if client.fail_requests >= request:
+                        client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
+                        splite_last_dec = client.time_blocked.rpartition('.')
+                        client.time_blocked = splite_last_dec[0]
+                    #ip_dict(match_IP)
+
+
+            for match in re.finditer(parse_php, line, re.S):
+                match_text = match.group()
+                #match_list.append(match_text)
+                search_IP = re.search(parse_IP, line, re.S)
+                match_IP = search_IP.group()
+                ##### PARSE THE TIME######
+                search_time = re.search(parse_time, line, re.S)
+                time_tried = search_time.group()
+                time_tried = convertTime(time_tried)
+                ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
+                for client in client_list:
+                    if client.ip_address == match_IP:
+                        client.fail_requests + 1
+                        notin_flag = True
+                if notin_flag == False:
+                    newclient = Client(match_IP, time_tried, 1, None)
+                    client_list.append(newclient)
+                notin_flag = False
+
+                    ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
+                    ##### CALL BLCOK IP METHOD #####
+                for client in client_list:
+                    if client.fail_requests >= request:
+                        client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
+                        splite_last_dec = client.time_blocked.rpartition('.')
+                        client.time_blocked = splite_last_dec[0]
+                    #ip_dict(match_IP)
+    file.close()
+
+def pareseSSHData(ssh_log_file, parse_ssh):
+    #match_list = []
+    notin_flag = False
+    with open(ssh_log_file, "r") as file:
+    for line in file:
+        for match in re.finditer(parse_ssh, line, re.S):
+            match_text = match.group()
+            #match_list.append(match_text)
+            search_IP = re.search(parse_IP, line, re.S)
+            match_IP = search_IP.group()
+            #print(match_IP)
+            ##### PARSE THE TIME######
+            search_time = re.search(parse_ssh_time, line, re.S)
+            time_tried = search_time.group()
+            time_tried = SSH_datetime(time_tried)
+            #print(time_tried)
+            ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
+            for client in client_list:
+                if client.ip_address == match_IP:
+                    client.fail_requests + 1
+                    notin_flag = True
+            if notin_flag == False:
+                newclient = Client(match_IP, time_tried, 1, None)
+                client_list.append(newclient)
+            notin_flag = False
+
+            ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
+            ##### CALL BLOCK IP METHOD #####
+            for client in client_list:
+                #print(client.fail_requests)
+                if client.fail_requests >= request:
+                    client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
+                    splite_last_dec = client.time_blocked.rpartition('.')
+                    client.time_blocked = splite_last_dec[0]
+            #ip_dict(match_IP)
+            #print match_list
+file.close()
+
+
+parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP)
+pareseSSHData(ssh_log_file, parse_ssh)
+
+
 
 #######################CHECK THROUGH THE LIST, SEE IF ANY CAN BE UNBLOCKED#######################
 if len(client_list) != 0:
     for c in client_list:
-        blocked = datetime.strptime(c.time_blocked, "%Y-%m-%d %H:%M:%S")
+        blocked = datetime.datetime.strptime(c.time_blocked, "%Y-%m-%d %H:%M:%S")
         if datetime.now() >= blocked:
             client_list.remove(c)
 #print("current client list is: " + client_list)         
