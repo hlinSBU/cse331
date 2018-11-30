f51812c49ffd57ccece99a1095236fc304e609e3
Unknown <hsinying.lin@stonybrook.edu>
2018-11-30 14:12:25 -0500

    1111

--- cse331.py
+++ cse331.py
@@ -37,14 +37,15 @@ time_now = str(strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 #######################CHECK THROUGH THE LIST, SEE IF ANY CAN BE UNBLOCKED#######################
 
 
-###########READ FILE############
+
+
+
+
+###########READ LOG_FILE############
 log_file_path = r"/var/log/apache2/access.log" #webserver log
 ssh_log_file =  r"/var/log/auth.log" #SSH LOG
 
 dict_check = {}
-
-
-
 
 parse_IP = '(\d+\.\d+\.\d+\.\d+)'
 parse_WP = '(\"POST\s.+200)'
@@ -79,6 +80,7 @@ def parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP):
                     new_client.fail_requests = 1
                     new_client.time_blocked = None
                     client_list.append(new_client)
+                notin_flag = False
 
                 ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
                 ##### CALL BLCOK IP METHOD #####
@@ -105,6 +107,7 @@ def parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP):
                     new_client.fail_requests = 1
                     new_client.time_blocked = None
                     client_list.append(new_client)
+                notin_flag = False
 
                 ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
                 ##### CALL BLCOK IP METHOD #####
@@ -131,6 +134,7 @@ def parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP):
                     new_client.fail_requests = 1
                     new_client.time_blocked = None
                     client_list.append(new_client)
+                notin_flag = False
 
                 ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
                 ##### CALL BLCOK IP METHOD #####
@@ -148,7 +152,28 @@ def pareseSSHData(ssh_log_file, parse_ssh):
             for match in re.finditer(parse_ssh, line, re.S):
                 match_text = match.group()
                 match_list.append(match_text)
-                match_IP = re.finditer(parse_IP, match_text, re.S)
+                match_IP = re.finditer(parse_IP, line, re.S)
+                                ##### PARSE THE TIME######
+                time_tried = re.finditer(parse_time, match_text, re.S)
+                ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
+                for client in client_list:
+                    if client.ip_address == match_IP:
+                        client.fail_requests + 1
+                        notin_flag = True
+                if notin_flag == False:
+                    new_client = Client()
+                    new_client.ip_address = match_IP
+                    new_client.first_login = time_tried
+                    new_client.fail_requests = 1
+                    new_client.time_blocked = None
+                    client_list.append(new_client)
+                notin_flag = False
+
+                ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
+                ##### CALL BLCOK IP METHOD #####
+                for client in client_list:
+                    if client.fail_requests >= request_window:
+                        blcok_ip(client.ip_address)
                 ip_dict(match_IP)
                 #print match_list
     file.close()
@@ -164,6 +189,9 @@ def blcok_ip(ip):
     cmd="iptables -A INPUT -s "+ip+" -j DROP"
     #print cmd
     subprocess.call(cmd,shell=True)
+
+
+############################WRITE TO JSON################################
 
 
 data_json_file.close()
