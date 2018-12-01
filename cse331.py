33c7e5bba73d5f66c0d680e21f4409d795d0d84d
Ubuntu <ubuntu@ip-172-31-16-54.us-east-2.compute.internal>
2018-12-01 22:10:38 +0000

    fixed typos etc need to fix regex

--- cse331.py
+++ cse331.py
@@ -16,7 +16,7 @@ class Client:
         self.time_blocked = time_blocked
         self.remove_blacklist = remove_blacklist
 
-with open("/usr/share/modips/data/clientdata.json") as data_json_file:
+with open("/home/ubuntu/Documents/cse331/myapp/clientdata.json") as data_json_file:
     data = json.load(data_json_file)
     admindata = data['admindata']
     request = admindata['requestlimit']
@@ -31,7 +31,7 @@ with open("/usr/share/modips/data/clientdata.json") as data_json_file:
         client_failed_requests = c['failedrequests']
         client_time_blocked = c['timeblocked']
         client_remove_blacklist = c['removeblacklist']
-        client = Client(clientIP, client_first_login, client_failed_requests, client_time_blocked, remove_blacklist)
+        client = Client(clientIP, client_first_login, client_failed_requests, client_time_blocked, client_remove_blacklist)
         client_list.append(client)
 
 
@@ -127,7 +127,7 @@ def SSH_datetime(input):
 
 ###########READ FILE############
 log_file_path = r"/var/log/apache2/access.log" #webserver log
-ssh_log_file =  r"/var/log/auth.log" #SSH LOG
+ssh_log_file =  r"/home/ubuntu/Documents/logparser/cse331/auth.log" #SSH LOG
 
 dict_check = {}
 
@@ -239,42 +239,42 @@ def pareseSSHData(ssh_log_file, parse_ssh):
     #match_list = []
     notin_flag = False
     with open(ssh_log_file, "r") as file:
-    for line in file:
-        for match in re.finditer(parse_ssh, line, re.S):
-            match_text = match.group()
-            #match_list.append(match_text)
-            search_IP = re.search(parse_IP, line, re.S)
-            match_IP = search_IP.group()
-            #print(match_IP)
-            ##### PARSE THE TIME######
-            search_time = re.search(parse_ssh_time, line, re.S)
-            time_tried = search_time.group()
-            time_tried = SSH_datetime(time_tried)
-            #print(time_tried)
-            ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
-            for client in client_list:
-                if client.ip_address == match_IP:
-                    client.fail_requests + 1
-                    notin_flag = True
-            if notin_flag == False:
-                newclient = Client(match_IP, time_tried, 1, None, False)
-                client_list.append(newclient)
-            notin_flag = False
+	    for line in file:
+	        for match in re.finditer(parse_ssh, line, re.S):
+	            match_text = match.group()
+	            #match_list.append(match_text)
+	            search_IP = re.search(parse_IP, line, re.S)
+	            match_IP = search_IP.group()
+	            #print(match_IP)
+	            ##### PARSE THE TIME######
+	            search_time = re.search(parse_ssh_time, line, re.S)
+	            time_tried = search_time.group()
+	            time_tried = SSH_datetime(time_tried)
+	            #print(time_tried)
+	            ##### CHECK THE IP ADDRESS IN THE client_list IF IT EXISTS ######
+	            for client in client_list:
+	                if client.ip_address == match_IP:
+	                    client.fail_requests + 1
+	                    notin_flag = True
+	            if notin_flag == False:
+	                newclient = Client(match_IP, time_tried, 1, None, False)
+	                client_list.append(newclient)
+	            notin_flag = False
 
-            ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
-            ##### CALL BLOCK IP METHOD #####
-            for client in client_list:
-                #print(client.fail_requests)
-                if client.fail_requests >= request:
-                    client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
-                    splite_last_dec = client.time_blocked.rpartition('.')
-                    client.time_blocked = splite_last_dec[0]
-            #ip_dict(match_IP)
-            #print match_list
-file.close()
+	            ##### IF THE NUMBERS OF REQUESTED IS OVER THE LIMITE #####
+	            ##### CALL BLOCK IP METHOD #####
+	            for client in client_list:
+	                #print(client.fail_requests)
+	                if client.fail_requests >= request:
+	                    client.time_blocked = str(datetime.datetime.now() + datetime.timedelta(minutes=request_window))
+	                    splite_last_dec = client.time_blocked.rpartition('.')
+	                    client.time_blocked = splite_last_dec[0]
+	            #ip_dict(match_IP)
+	            #print match_list
+    file.close()
 
 
-parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP)
+#parseApacheData(log_file_path, parse_WP, parse_jm, parse_php, parse_IP)
 pareseSSHData(ssh_log_file, parse_ssh)
 
 
@@ -300,7 +300,7 @@ for c in client_list:
         "ipaddress" : c.ip_address,
         "firstlogin" : c.first_login,
         "failedrequests" : c.fail_requests,
-        "timeblocked" : c.time_blocked
+        "timeblocked" : c.time_blocked,
         "removeblacklist": c.remove_blacklist
     }
     client.append(x)
